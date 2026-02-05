6b05f101d536f0a941857c4db93f1679cc3dea8d
Santiago Sanchez Hernandez <122324891+garbanzo12@users.noreply.github.com>
2026-02-05 11:12:40 -0500

    Comentario en babel.config
    Comentario en babel.config

--- babel.config.cjs
+++ babel.config.cjs
@@ -11,6 +11,7 @@ module.exports = {
   plugins: [
     ["@babel/plugin-proposal-decorators", { legacy: true }], // Plugin para balbe para que soporte los decorators
     ["@babel/plugin-proposal-class-properties", { loose: true }],
-    ["@babel/plugin-transform-private-methods",{loose: true}]
+    ["@babel/plugin-transform-private-methods",{loose: true}] // Plugin para soportar metodos privados
+  
   ]
 };
